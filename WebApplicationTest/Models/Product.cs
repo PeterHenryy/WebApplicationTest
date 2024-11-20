b3f52c6631e0cff4590cd94b5d1f825039898ed7
PeterHenryy <snakenoxz@gmail.com>
2024-11-20 13:36:43 -0300

    Add project files.

---
+++ WebApplicationTest/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationTest.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
