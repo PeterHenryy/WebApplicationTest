3840bd60e9fcd81ef459ce20ec7cd916731b47d7
PeterHenryy <snakenoxz@gmail.com>
2024-11-25 18:17:25 -0300

    make it better

--- WebApplicationTest/Models/Identity/AppUser.cs
+++ WebApplicationTest/Models/Identity/AppUser.cs
@@ -6,10 +6,10 @@ namespace WebApplicationTest.Models.Identity
     {
         public int CompanyID { get; set; }
         public bool IsAdmin { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public string Password { get; set; }
-        public string ProfilePicture { get; set; }
+        public string? ProfilePicture { get; set; }
         public double UserRewardPoints { get; set; }
         public bool HasCreditCard { get; set; }
     }
